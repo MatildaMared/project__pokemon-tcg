75f35df6e2d79002dcb8899a8babe5d1614d619d
Matilda Mared <matildamared@live.se>
2023-07-28 09:42:43 +0200

    add prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import React from "react"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: 'Pokémon TCG',
-  description: 'Browse Pokémon TCG cards',
+  title: "Pokémon TCG",
+  description: "Browse Pokémon TCG cards",
 }
 
 export default function RootLayout({

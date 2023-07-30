a976ffdd92ecf027488bd45446e5bf3a511dd40c
Matilda Mared <matildamared@live.se>
2023-07-30 08:07:37 +0200

    add styled components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <h1>Pokemon TCG</h1>
       <Link href={"/series"}>Series</Link>
     </main>
--- src/app/series/page.tsx
+++ src/app/series/page.tsx
@@ -3,10 +3,10 @@ import { series } from "@/types/Series";
 
 export default async function SetsPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main>
       <h1>Pokemon TCG Sets</h1>
       <Link href={"/"}>Home</Link>
-      <ul className="flex flex-col">
+      <ul>
         {series.map((series) => (
           <Link key={series} href={`/series/${series}`}>
             {series}

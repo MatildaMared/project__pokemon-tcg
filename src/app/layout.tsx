1e82dfd984accbfadf36281c6c3da5c2b7c3765c
Matilda Mared <matildamared@live.se>
2023-07-30 07:55:29 +0200

    uninstall tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,22 @@
-import "./globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import React from "react"
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import React from "react";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pokémon TCG",
   description: "Browse Pokémon TCG cards",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
